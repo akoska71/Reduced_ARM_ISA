24237be6ce28b87e7ed85e34d736ec5b52fb1fd4
akoska71 <165975997+akoska71@users.noreply.github.com>
2024-09-28 17:14:32 -0400

    Update branch_en.v

--- src/branch_en.v
+++ src/branch_en.v
@@ -1,17 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Author: AJ Koska
 // 
 // Create Date: 08/25/2024 05:41:42 PM
-// Design Name: 
 // Module Name: branch_en
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Description: branch request unit to check if a branch should be requested based
+// on the current Instruction
 // 
-// Dependencies: 
 // 
 // Revision:
 // Revision 0.01 - File Created
@@ -38,14 +33,14 @@ module branch_en # (parameter OP_BITS = 5, parameter COND_BITS = 4)(
     
     always @ * begin
         case(branch_type)
-        3'b000: x_en = 1'b1;
-        3'b001: x_en = &{!cond_code[0], n_xnor_z};
-        3'b010: x_en = cond_code[0];
-        3'b011: x_en = n_xnor_z;
-        3'b100: x_en = !n_xnor_z;
-        3'b101: x_en = !cond_code[0];
-        3'b110: x_en = !(&{n_xnor_z, !cond_code[0]});
-        3'b111: x_en = 1'b1;
+            3'b000: x_en = 1'b1; //branch
+            3'b001: x_en = &{!cond_code[0], n_xnor_z}; //b.gt
+            3'b010: x_en = cond_code[0]; //b.eq
+            3'b011: x_en = n_xnor_z; //b.ge
+            3'b100: x_en = !n_xnor_z; //b/lt
+            3'b101: x_en = !cond_code[0]; //b.ne
+            3'b110: x_en = !(&{n_xnor_z, !cond_code[0]}); //b.le
+            3'b111: x_en = 1'b1; //halt
         endcase
     end
     
