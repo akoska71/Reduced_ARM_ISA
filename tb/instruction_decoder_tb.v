aeef85f5e9edc1a1a107651c204c2c34abe9f588
akoska71 <165975997+akoska71@users.noreply.github.com>
2024-11-12 17:53:56 -0500

    Update instruction_decoder_tb.v

--- tb/instruction_decoder_tb.v
+++ tb/instruction_decoder_tb.v
@@ -9,7 +9,8 @@
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description:Testbench to observe control signals being generated
+// by the Instruction Decoder
 // 
 // Dependencies: 
 // 
@@ -37,7 +38,8 @@ module instruction_decoder_tb();
     
     instruction_decoder dut (.instr(instr), .cond_update(cond_update), .mem_wr(mem_wr), .mem_rd(mem_rd),
                              .reg_wr(reg_wr), .wSel(wSel), .aSel(aSel), .bSel(bSel), .imm5(imm5), .imm8(imm8), .imm11(imm11));
-    
+
+    //Monitor signals generated depending on the operation sent to the ALU.
     initial begin
         instr[10:0] = 0;
         $monitor("Instr:%d\nCon:%d  Mem_wr:%b  Mem_rd:%b  Reg_wr:%b \nwSel:%b aSel:%b bSel:%b imm5:%b imm8:%b imm11:%b\n",
